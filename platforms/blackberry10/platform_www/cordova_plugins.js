26545df29265c1716d1a29d6fc6efa983665c512
mflayhart <mflayhart@gmail.com>
2015-11-23 15:20:10 -0600

    dependency updates

--- platforms/blackberry10/platform_www/cordova_plugins.js
+++ platforms/blackberry10/platform_www/cordova_plugins.js
@@ -1,5 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {}
